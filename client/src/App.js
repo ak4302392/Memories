91177f9c88393caa3eb41099330602a5fbdfa5c8
AMIT KUMAR <ak4302392@gmail.com>
2022-06-18 16:33:05 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+
+import memories from "./images/memories.png";
+const App = () => {
+  return (
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="app-title" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
