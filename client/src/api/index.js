f31370281eb7a93ed992b025a259de031b7363d6
AMIT KUMAR <ak4302392@gmail.com>
2022-06-27 15:18:56 +0530

    updated the url of server at the client api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-
-const API = axios.create({ baseURL: "http://localhost:5000" });
-
-// const url = "https://memories43023.herokuapp.com/posts";
+const url = "https://memories43023.herokuapp.com";
+const API = axios.create({ baseURL: url });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
